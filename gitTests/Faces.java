1fda66402bb85fd254432fc123b514fc614d3e1b
Allarux <gameplayer0@gmail.com>
2018-04-06 21:10:21 -0700

    init upload

---
+++ gitTests/Faces.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ * Assignment: Lab 4 Faces
+ *
+ */
+
+package faces;
+
+public class Faces {
+
+    public static void main(String[] args) {
+        // DRAWS 4 FACES BY USING METHODS
+        drawFace1();
+        System.out.println("\n"); // ADD WHITE SPACE BETWEEN FACES TO MAKE IT EASIER TO READ
+
+        drawFace2();
+        System.out.println("\n");
+
+        drawFace3();
+        System.out.println("\n");
+
+        drawFace4();
+    }
+
+    // CREATES PROFILES OF DIFFERENT FACIAL FEATURES
+    public static void drawFace1() {
+        drawHair1();
+        drawEyes1();
+        drawMouth1();
+    }
+
+    public static void drawFace2() {
+        drawHair2();
+        drawEyes2();
+        drawMouth2();
+    }
+
+    public static void drawFace3() {
+        drawHair2();
+        drawEyes1();
+        drawMouth2();
+    }
+
+    public static void drawFace4() {
+        drawHair1();
+        drawEyes2();
+        drawMouth2();
+    }
+
+    // HAIR TYPES
+    public static void drawHair1() {
+        System.out.println("  ######  ");
+        System.out.println(" ######## ");
+        System.out.println("/#__  __#\\");
+    }
+
+    public static void drawHair2() {
+        System.out.println("  @@@@@@  ");
+        System.out.println(" @@@@@@@@ ");
+        System.out.println("@@__  __@@");
+    }
+
+    // EYE TYPES
+    public static void drawEyes1() {
+        System.out.println("| []  [] |");
+        System.out.println("\\   /\\   /");
+    }
+
+    public static void drawEyes2() {
+        System.out.println("{ <>  <> }");
+        System.out.println("\\   ##   /");
+    }
+
+    // MOUTH TYPES
+    public static void drawMouth1() {
+        System.out.println(" \\  ##  / ");
+        System.out.println("  \\ == /  ");
+        System.out.println("   \\__/   ");
+    }
+
+    public static void drawMouth2() {
+        System.out.println(" \\      / ");
+        System.out.println("  \\ ~~ /  ");
+        System.out.println("   \\__/   ");
+    }
+    
+// END OF PUBLIC CLASS
+}
